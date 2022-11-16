d398778f49a541603866e2e0a435b1052097f7e7
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-11-16 13:28:45 -0800

    Tokenization

---
+++ get_tokens.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * get_tokens - Gets the tokens
+ * @line: The line to get tokenize
+ * @nread: The number of tokens to read
+ *
+ * Return: 0 or -1
+ */
+
+char **get_tokens(char *line, ssize_t nread)
+{
+	char *cbuffer, *delim = " \n";
+	int ntoken = 0, i = 0;
+	char *token, **tokens;
+
+	cbuffer = malloc(sizeof(char) * nread);
+
+	strcopy(cbuffer, buffer);
+
+	token = strtok(buffer, delim);
+	while (token != NULL)
+	{
+		ntoken++;
+		token = strtok(NULL, delim);
+	}
+	ntoken++;
+	tokens = malloc(sizeof(char *) * ntoken);
+	token = strtok(cbuffer, delim);
+	while (token != NULL)
+	{
+		tokens[i] = malloc(sizeof(char) * strlen(token));
+		strcpy(tokens[i], token);
+		i++;
+		token = strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}

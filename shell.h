4430b75e8241cd2a0de1a41e4661d798b5bfc6cc
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-11-17 11:38:36 -0800

    Header file

--- shell.h
+++ shell.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+char** get_tokens(char *buffer, ssize_t nread);
+char *read_line(ssize_t *nread);
+int execute_cmd(char **tokens);
 
 #endif

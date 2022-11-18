1cf42853defc97b8c3c887e0d72eae007f3cd002
Ndeye Aminata Sall <aminatandeye15@gmail.com>
2022-11-18 10:53:34 -0800

    Header file

--- shell.h
+++ shell.h
@@ -10,14 +10,10 @@
 
 char** get_tokens(char *buffer, ssize_t nread);
 char *read_line(ssize_t *nread);
-<<<<<<< HEAD
-int execute_cmd(char **tokens);
+void execute_cmd(char **tokens);
 char *_strcpy(char *dest, const char *src);
 int _strlen(const char *str);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
-=======
-void execute_cmd(char **tokens);
->>>>>>> 8a0a6f2beef6939e6d0b8bcfd6ea50fa152d3830
 
 #endif
